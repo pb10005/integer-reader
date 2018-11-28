ca4b4a487115677628a7bcd1dd449fb3491feebe
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-28 14:10:38 +0900

    Add nyc config

--- test/test-unit-en.ts
+++ test/test-unit-en.ts
@@ -2,6 +2,12 @@ import { assert } from 'chai';
 import { describe, it } from 'mocha';
 import { EnglishReader } from '../src/EnglishReader';
 
+describe('new EnglishReader(0).num', () => {
+  it('returns 0', () => {
+    assert.equal(0, new EnglishReader(0).num);
+  });
+});
+
 describe('new EnglishReader(0).read()', () => {
   it('returns zero', () => {
     assert.equal('zero', new EnglishReader(0).read());
--- test/test-unit-ja.ts
+++ test/test-unit-ja.ts
@@ -2,6 +2,12 @@ import { assert } from 'chai';
 import { describe, it } from 'mocha';
 import { JapaneseReader } from '../src/JapaneseReader';
 
+describe('new EnglishReader(0).num', () => {
+  it('returns 0', () => {
+    assert.equal(0, new JapaneseReader(0).num);
+  });
+});
+
 describe('new JapaneesReader(0).read()', () => {
   it('returns 〇', () => {
     assert.equal('〇', new JapaneseReader(0).read());
